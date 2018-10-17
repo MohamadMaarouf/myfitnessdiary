e0fc9c7414d53db36a04904e59e66f101c53d005
Chris Conlon <chris.conlon1993@gmail.com>
2018-10-17 12:18:39 -0400

    For some reason merge missed forms.py, re-adding it now

---
+++ modules/forms.py
@@ -0,0 +1,24 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, SelectField
+from wtforms.validators import DataRequired, Email, Length, EqualTo
+
+
+class Login(FlaskForm):
+    email = StringField('Email: ', validators=[DataRequired(), Email()])
+    password = PasswordField('Password: ', validators=[
+                             DataRequired(), Length(min=6, max=255)])
+    submit = SubmitField('Login')
+
+
+class Registration(FlaskForm):
+    first_name = StringField('First Name: ', validators=[DataRequired()])
+    last_name = StringField('Last Name: ', validators=[DataRequired()])
+    user_type = SelectField('Account Type: ', validators=[DataRequired()], choices=[
+                            ('stu', 'Student'), ('fac', 'Faculty'), ('spo', 'Sponsor')])
+    v_key = StringField('Verification Key: ', validators=[DataRequired()])
+    email = StringField('Email: ', validators=[DataRequired(), Email()])
+    password = StringField('Password: ', validators=[
+                           DataRequired(), Length(min=6)])
+    confirm = StringField('Confirm Password:  ', validators=[
+                          DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register ', validators=[DataRequired()])
