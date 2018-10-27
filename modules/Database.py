7b2a9f4701bd5c92b98cab135f6b768a63382903
Chris Conlon <chris.conlon1993@gmail.com>
2018-10-27 14:53:58 -0400

    Passwords encrypted before query's

--- modules/Database.py
+++ modules/Database.py
@@ -1,5 +1,6 @@
 ''' This module will build our database object. '''
 import pymysql
+import hashlib
 
 
 class Database():
@@ -13,22 +14,40 @@ class Database():
         credentials = False
         sql = ('SELECT * FROM  users WHERE  email="'+email+'"')
         row = self.query('PULL', sql)
-        if(len(row) != 0 and row[0][1] == email and row[0][2] == password):
+        if(len(row) != 0 and row[0][1] == email and row[0][2] == self.encrypt(password)):
             credentials = True
         return (credentials)
 
     def register(self, first, last, uType, vKey, email, password):
+        password = self.encrypt(password)
         arg = (0, email, password, uType)
         sql = "INSERT INTO users (user_id, email, password, role) VALUES (%s, %s, %s, %s)"
-        print(arg)
         self.query('PUSH', sql, arg)
-        print("PULL", "Select * from users")
 
     def connect(self):
         db = pymysql.connect(host=(self.IP), user=self.USER,
                              password=self.DB_PASS, db=self.DATABASE)
         cursor = db.cursor()
         return (db, cursor)
+
+    def encrypt(self, toEncrypt):
+        sha = hashlib.sha256()
+        sha.update(toEncrypt.encode('utf-8'))
+        encrypted = sha.hexdigest()
+        return encrypted
+
+    '''
+    The query function will be the bread and butter of our database object
+    
+    arguments:
+        self: required for object construction
+        qType: short for query type. This will tell the databse if you are pulling ("PULL") down data 
+            to work with, (i.e logging in) or pushing ("PUSH") data to the database (i.e registration) 
+
+    return:
+        if pulling down data it returns a tuple that was gained via your query
+        if pushing data there is no return
+    '''
 
     def query(self, qType, statement, *args):
         database, cursor = self.connect()
