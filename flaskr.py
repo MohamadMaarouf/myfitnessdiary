50c64017cedd8fffad494c89f5f6d9d369dc98bb
Djaekle <djaekle123@gmail.com>
2018-10-29 13:59:18 -0400

    nothing

--- flaskr.py
+++ flaskr.py
@@ -87,27 +87,6 @@ def registration():
             db.register(first, last, user_type, vKey, email, pswrd)
             print(db.query('PULL', "Select * from users"))
           
-        '''db = pymysql.connect(host=IP, user='root',
-                             password=pas, db='internreq')
-        c = db.cursor()
-        c.execute('Select * from users where email="' +
-                  email+'"')
-        l = c.fetchall()  # With this tuple we can parse for information to assign each user
-        # ((1, 'chris.conlon1993@gmail.com', 'password', 'faculty admin'),)
-
-        if(len(l) == 0):
-            sql = "INSERT INTO users (user_id, email, password, role) VALUES(%s,%s,%s,%s)"
-            c.execute(sql, (int(0), email, pswrd, user_type))
-            c.execute("Select * from users")
-            print(c.fetchall())
-
-            # insert to student, faculty, sponsor tables
-            sql = "SELECT user_id FROM users WHERE email LIKE '"+email+"'"
-            c.execute(sql)
-            user_id = c.fetchall()
-            sql = "INSERT INTO "+user_type+"(user_id, first_name, last_name) VALUES(%s,%s,%s)"
-            c.execute(sql, (user_id, first_name, last_name))'''
-        
         else:
             flash("Email address already used! Please Login.", 'danger')
             return redirect('/registration')
